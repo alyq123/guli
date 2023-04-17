3edc14d19edadad25018ea932576e5b15fb3a914
yingqi <1533912325@qq.com>
2023-04-17 11:28:28 +0800

    第七章.笔记

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/listener/SubjectExcelListener.java
@@ -0,0 +1,78 @@
+package com.atguigu.eduservice.listener;
+
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.event.AnalysisEventListener;
+import com.atguigu.eduservice.entity.EduSubject;
+import com.atguigu.eduservice.entity.excel.SubjectData;
+import com.atguigu.eduservice.service.EduSubjectService;
+import com.atguigu.servicebase.exceptionhandler.GuliException;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
+/**
+ * @description:
+ * @author: lyq
+ * @date: 2023/4/16
+ * @Copyright： 日拱一卒 功不唐捐！
+ */
+public class SubjectExcelListener extends AnalysisEventListener<SubjectData> {
+
+    public EduSubjectService subjectService;
+    public SubjectExcelListener() {}
+    public SubjectExcelListener(EduSubjectService subjectService) {
+        this.subjectService = subjectService;
+    }
+
+    //一行一行读取excel内容(从第二行开始读取,第一行是表头,该方法不会读取第一行)
+    @Override
+    public void invoke(SubjectData subjectData, AnalysisContext analysisContext) {
+        //1.判断文件是否为空
+        if (subjectData == null) {
+            throw new GuliException(20001, "文件数据为空");
+        }
+
+        //2.一行一行读取,每次读取有两个值,第一个值是一级分类,第二个值是二级分类
+        //①添加一级分类
+        EduSubject existOneSubject = this.existOneSubject(subjectService, subjectData.getOneSubjectName());
+        if (existOneSubject == null) { //一级分类没有重复,进行添加
+            existOneSubject = new EduSubject();
+            existOneSubject.setParentId("0");
+            existOneSubject.setTitle(subjectData.getOneSubjectName());
+            subjectService.save(existOneSubject);
+        }
+        //②添加二级分类
+        //先获得一级分类的id
+        String pid = existOneSubject.getId();
+        //再添加二级分类
+        EduSubject existTwoSubject = this.existTwoSubject(subjectService,subjectData.getTwoSubjectName(), pid);
+        if (existTwoSubject == null) { //二级分类没有重复,进行添加
+            existTwoSubject = new EduSubject();
+            existTwoSubject.setParentId(pid);
+            existTwoSubject.setTitle(subjectData.getTwoSubjectName());
+            subjectService.save(existTwoSubject);
+        }
+    }
+
+    //判断一级分类是否重复
+    private EduSubject existOneSubject(EduSubjectService subjectService, String name) {
+        QueryWrapper<EduSubject> wrapper = new QueryWrapper<>();
+        wrapper.eq("title", name);
+        wrapper.eq("parent_id", "0");
+        EduSubject oneSubject = subjectService.getOne(wrapper);
+        return oneSubject;
+    }
+
+    //判断二级分类是否重复
+    private EduSubject existTwoSubject(EduSubjectService subjectService, String name, String pid) {
+        QueryWrapper<EduSubject> wrapper = new QueryWrapper<>();
+        wrapper.eq("title", name);
+        wrapper.eq("parent_id", pid);
+        EduSubject twoSubject = subjectService.getOne(wrapper);
+        return twoSubject;
+    }
+
+    //读取完成之后执行这个方法
+    @Override
+    public void doAfterAllAnalysed(AnalysisContext analysisContext) {
+    }
+}
+
